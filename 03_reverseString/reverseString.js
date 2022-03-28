e7936f114cc778db181e606384e30a54ce90506b
Syed Mujahid Islam <syedmujahidislam@gmail.com>
2022-03-28 19:28:12 +0600

    Completed & tested reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    reversedArray = array.reverse()
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
