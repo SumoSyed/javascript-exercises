b983a1bd6758757b3d13acfb2cbd894e532967e8
Syed Mujahid Islam <syedmujahidislam@gmail.com>
2022-04-15 22:28:55 +0600

    Completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // start = 400;
+    // end = 10;
+    let sum = 0;
+    if (start < 0 || end < 0){
+        return "ERROR"
+    }else if(!Number.isInteger(end) || !Number.isInteger(start)){
+        return "ERROR"
+    }else if(end < start){
+        let holder = start;
+        start = end;
+        end = holder;
+    }
 
+    for (let i = start; i <= end; i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
